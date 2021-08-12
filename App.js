7cd75e6f4cc02363cb81d7c485c31562f8b8cb73
DESKTOP-BTQFQHB\samsung <ldk4651@gmail.com>
2021-08-12 22:51:34 +0900

    날씨앱을 만들기 위해 getting the fucking weather 만들기

--- App.js
+++ App.js
@@ -1,35 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import Loading from './Loading';
 
 export default function App() {
   return (
-    <View style = {styles.container}>
-      <View style = {styles.yellowview}>
-        <Text>hello</Text>
-        <Text>hello</Text>
-      </View>
-      <View style = {styles.blueview}></View>
-    </View>
+
+    <Loading />
 
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection : 'column',
-    backgroundColor: 'white'
-  },
-  yellowview: {
-    flex: 1,
-    flexDirection : 'row',
-    backgroundColor: 'yellow'
-  },
-  blueview: {
-    flex: 1,
-    backgroundColor: 'blue'
-  }
-
-});
