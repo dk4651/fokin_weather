2610e7728135d85a69f9e94c6a36edc6111e8225
DESKTOP-BTQFQHB\samsung <ldk4651@gmail.com>
2021-08-12 22:30:35 +0900

    View와 style에서 flex와 flexDirection 에 대한 예제

--- App.js
+++ App.js
@@ -2,20 +2,34 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <View style = {styles.yellowview}>
+        <Text>hello</Text>
+        <Text>hello</Text>
+      </View>
+      <View style = {styles.blueview}></View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection : 'column',
+    backgroundColor: 'white'
   },
+  yellowview: {
+    flex: 1,
+    flexDirection : 'row',
+    backgroundColor: 'yellow'
+  },
+  blueview: {
+    flex: 1,
+    backgroundColor: 'blue'
+  }
+
 });
